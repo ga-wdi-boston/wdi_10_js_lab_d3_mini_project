a89c3b5bc32ae80e8beac6f449d63c1299f777c6
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-14 11:20:43 -0400

    created the Names controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 D3Visualizer::Application.routes.draw do
+  get "names/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
