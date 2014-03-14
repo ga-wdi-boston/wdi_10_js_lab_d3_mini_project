b74c1e6500689ed461092fca14a0155087a1778a
Praggya Rustagi Barretto <praggya@gmail.com>
2014-03-14 11:27:46 -0400

    created the rake task to import name data

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,4 +1,8 @@
 class NamesController < ApplicationController
   def index
+    @names = Name.all
+    respond_to do |format|
+    format.html
+    format.json {render json: @names, root: false }
   end
 end
---
+++ lib/tasks/import.rake
@@ -0,0 +1,6 @@
+namespace :import do
+  desc "TODO"
+  task baby_name_data: :environment do
+  end
+
+end
