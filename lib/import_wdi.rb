46f61eb1ed4e04b85484389834c0ef057a001ad8
Jordan Morano <jordan.morano@gmail.com>
2014-03-14 14:59:29 -0400

    Ran rake task

--- lib/import_wdi.rb
+++ lib/import_wdi.rb
@@ -20,13 +20,12 @@ class ImportWdi
 
   def self.import_wdi
     data = CSV.read("#{Rails.root}/data/WDI_Data.csv")
-    years = [1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013]
+    years = [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013]
     indicators = ["SP.POP.TOTL", "IT.NET.USER.P2","IT.NET.BBND", "IT.NET.BBND.P2", "IT.CEL.SETS", "IT.CEL.SETS.P2"]
 
     data.each_with_index do |line, i|
-      if i !=0
-        country = Country.find_by_code(line[1])
-
+      country = Country.find_by_code(line[1])
+      if i !=0 && country && line[2] && line[3]
         if indicators.include?(line[3])
           indicator = Indicator.find_by_code(line[3]) || Indicator.create!({
               name: line[2],
