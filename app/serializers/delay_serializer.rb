c4126b26f4239c26678798e05d3b859bc697b490
K Mak <karen.mak@stx.oxon.org>
2014-03-18 10:18:52 -0400

    playing with aggregate queries

--- app/serializers/delay_serializer.rb
+++ app/serializers/delay_serializer.rb
@@ -1,3 +1,33 @@
 class DelaySerializer < ActiveModel::Serializer
-  attributes :day_of_week
+  attributes :day_of_week,
+  # :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
+
+  # def monday
+  #   @delays = Delay.count(:conditions => "day_of_week = 1")
+  # end
+
+  # def tuesday
+  #   @delays = Delay.count(:conditions => "day_of_week = 2")
+  # end
+
+  # def wednesday
+  #   @delays = Delay.count(:conditions => "day_of_week = 3")
+  # end
+
+  # def thursday
+  #   @delays = Delay.count(:conditions => "day_of_week = 4")
+  # end
+
+  # def friday
+  #   @delays = Delay.count(:conditions => "day_of_week = 5")
+  # end
+
+  # def saturday
+  #   @delays = Delay.count(:conditions => "day_of_week = 6")
+  # end
+
+  # def sunday
+  #   @delays = Delay.count(:conditions => "day_of_week = 7")
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 D3Visualizer::Application.routes.draw do
   resources :carriers, :only => [:index, :show]
   root "carriers#index"
+  resources :delays, :only => [:index]
 
 end
