c7418e3a0c261096d4f3f92473ed8ecde2860ec4
Anna Tsykalova <tsyan@users.noreply.github.com>
2014-03-20 09:47:57 -0400

    all data is imported correctly

--- lib/import/import_employees.rb
+++ lib/import/import_employees.rb
@@ -74,23 +74,4 @@ class ImportEmployees
 
   end
 
-  #####################################
-
-
-  def self.import_recidivism
-    data =  CSV.read("#{Rails.root}/data/ppus07st06.csv")
-
-    data.each do |row|
-      state_name = row[1]
-
-      if STATE_NAMES.include?(state_name)
-        state_reciv =  StateRecividism.create!(name: state_name, pop_at_risk: row[3], returned_to_jail: row[4], percent: row[5])
-
-        puts "Added #{state_reciv.inspect}"
-        # puts "name: #{row[1]}, pop_at_risk: #{row[3]}, returned_to_jail: #{row[4]}, percentage: #{row[5]},  calc_percent = #{(row[4].to_f/row[3].to_f)*100}"
-      end
-
-    end
-
-  end
 end
